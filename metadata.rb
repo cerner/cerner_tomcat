891760b06c55fc41ab8a32189a0441e0b649145c
Bryan Baugher <bryan.baugher@cerner.com>
2016-04-19 12:25:35 -0500

    Added issues_url and source_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ maintainer_email 'Bryan.Baugher@Cerner.com'
 license 'All rights reserved'
 description 'Installs/Configures tomcat'
 long_description 'This cookbook is meant to install and configure an instance of tomcat'
-
+issues_url 'https://github.com/cerner/cerner_tomcat/issues'
+source_url 'https://github.com/cerner/cerner_tomcat'
 name 'cerner_tomcat'
 
 supports 'centos'
