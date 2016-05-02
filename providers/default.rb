ed2794950ab15d8cd12ca5818c567ccca4960e08
Bryan Baugher <bryan.baugher@cerner.com>
2016-05-02 12:16:00 -0500

    Ensured variables used to deleted directories are not empty strings

--- providers/default.rb
+++ providers/default.rb
@@ -227,6 +227,12 @@ action :install do
 end
 
 action :uninstall do
+  # Since we are recusively deleting directories guard against empty string which
+  # could result in deleting more then what we want
+  fail "base_dir cannot be empty string" if new_resource.base_dir.empty?
+  fail "instance_name cannot be empty string" if new_resource.instance_name.empty?
+  fail "log_dir cannot be empty string" if new_resource.log_dir.empty?
+
   service "tomcat_#{new_resource.instance_name}" do
     supports status: true, restart: true
     action [:disable, :stop]
